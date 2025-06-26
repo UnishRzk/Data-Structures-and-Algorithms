1ee8bd4ab6b7e3a62e887aff39cd344291647e26
UnishRzk <unishrajak0@gmail.com>
2025-06-26 21:30:20 +0545

    Singly linked list

---
+++ src/com/unish/datastructures/linkedlist/singly/CustomLinkedList.java
@@ -0,0 +1,56 @@
+package com.unish.datastructures.linkedlist.singly;
+
+public class CustomLinkedList {
+
+    private Node head;
+    private Node tail;
+
+    private int size;
+
+    public CustomLinkedList() {
+        this.size=0;
+    }
+
+    public void insertFirst(int val){
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+
+        if (tail== null){
+            tail = head;
+        }
+
+        size+=1;
+    }
+
+    public void display(){
+        Node temp = head;
+
+        while (temp != null){
+            System.out.print(temp.value+ " -> ");
+            temp = temp.next;
+        }
+        System.out.println("[END]");
+    }
+
+    private class Node{
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "[" +
+                      value +
+                    "]";
+        }
+    }
+}
---
+++ src/com/unish/datastructures/linkedlist/singly/InBuiltExample.java
@@ -0,0 +1,15 @@
+package com.unish.datastructures.linkedlist.singly;
+
+import java.util.LinkedList;
+
+public class InBuiltExample {
+    public static void main(String[] args) {
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        linkedList.add(64);
+        linkedList.add(87);
+        linkedList.add(65);
+
+    }
+}
